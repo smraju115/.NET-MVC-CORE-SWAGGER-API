397a5b7ada700724be058ec540ffbec78c2d7cfa
smraju115 <smraju115@gmail.com>
2024-11-02 10:24:39 +0600

    Add project files.

---
+++ ProductsAPI/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Data;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductCategoriesController : ControllerBase
+    {
+        private readonly ProductsAPIContext _context;
+
+        public ProductCategoriesController(ProductsAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductCategories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategory()
+        {
+            return await _context.ProductCategory.Include(pc => pc.Products).ToListAsync();
+        }
+
+        // GET: ProductCategories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductCategory>> GetProductCategory(int id)
+        {
+            var productCategory = await _context.ProductCategory.Include(pc => pc.Products).SingleAsync(pc => pc.ProductCategoryID == id);
+
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return productCategory;
+        }
+
+        // PUT: ProductCategories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductCategory(int id, ProductCategory productCategory)
+        { 
+           
+            if (id != productCategory.ProductCategoryID)
+            {
+                return BadRequest();
+            }
+            var existing = await _context.ProductCategory.FirstOrDefaultAsync(x => x.ProductCategoryID == id);
+            if (existing == null) { return NotFound(); }
+            existing.Name = productCategory.Name;
+            _context.Database.ExecuteSqlInterpolated($"DELETE FROM Product WHERE ProductCategoryID={id}");
+            if(productCategory.Products !=null)
+            {
+                foreach (var p in productCategory.Products)
+                {
+                    _context.Product.Add(new Product { ProductCategoryID = id, Name = p.Name, ProductNumber = p.ProductNumber, Color = p.Color, StandardCost = p.StandardCost, ListPrice = p.ListPrice, Size = p.Size, Weight = p.Weight });
+                }
+            }
+            
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: ProductCategories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductCategory>> PostProductCategory(ProductCategory productCategory)
+        {
+            _context.ProductCategory.Add(productCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductCategory", new { id = productCategory.ProductCategoryID }, productCategory);
+        }
+
+        // DELETE: ProductCategories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductCategory(int id)
+        {
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductCategory.Remove(productCategory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductCategoryExists(int id)
+        {
+            return _context.ProductCategory.Any(e => e.ProductCategoryID == id);
+        }
+    }
+}
---
+++ ProductsAPI/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductsAPI.Data;
+using System.Text.Json.Serialization;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ProductsAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductsAPIContext") ?? throw new InvalidOperationException("Connection string 'ProductsAPIContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    
+
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(setUp =>
+{
+    setUp.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseCors("CorsPolicy");
+app.MapControllers();
+
+app.Run();
